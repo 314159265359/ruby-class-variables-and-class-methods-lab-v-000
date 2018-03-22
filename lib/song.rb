fe5cb43c5164ec6aab819194aef8888f084dbd05
Karen G <kgraves0701@gmail.com>
2018-03-22 23:29:52 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -37,22 +37,6 @@ class Song
     genre_counter.each do |k, v|
     k=>v
    end
-
-   # sample array
-a=["aa","bb","cc","bb","bb","cc"]
-
-# make the hash default to 0 so that += will work correctly
-b = Hash.new(0)
-
-# iterate over the array, counting duplicate entries
-a.each do |v|
-  b[v] += 1
-end
-
-b.each do |k, v|
-  puts "#{k} appears #{v} times"
-end
-
   end
 
 end
