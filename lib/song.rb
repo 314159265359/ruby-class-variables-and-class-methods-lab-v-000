831eca234d792c34facd94eb579938972bcc7419
Karen G <kgraves0701@gmail.com>
2018-03-22 23:29:44 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -28,6 +28,30 @@ class Song
   end
 
   def genre_count
+    genre_counter = Hash.new{ |h, k| h[k] = [] }
+
+    @@genres.each do |v|
+    genre_counter[v] += 1
+    end
+
+    genre_counter.each do |k, v|
+    k=>v
+   end
+
+   # sample array
+a=["aa","bb","cc","bb","bb","cc"]
+
+# make the hash default to 0 so that += will work correctly
+b = Hash.new(0)
+
+# iterate over the array, counting duplicate entries
+a.each do |v|
+  b[v] += 1
+end
+
+b.each do |k, v|
+  puts "#{k} appears #{v} times"
+end
 
   end
 
